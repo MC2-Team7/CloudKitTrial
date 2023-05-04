3b7ff946c730dfb3a4c6e456941f345ac89570e8
Ung <jjw3435@naver.com>
2023-05-04 16:08:20 +0900

    Initial Commit

---
+++ CloudKitTrial/CloudKitTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitTrialApp.swift
+//  CloudKitTrial
+//
+//  Created by JaeUngJang on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
